ac0066874f2c10df97dd97920936f0f17613add6
rodinkim <xday6666@gmail.com>
2023-04-12 17:27:45 +0900

    [test] refresh token redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,12 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
-
+// const redisClient = createClient({
+//     socket: {
+//       host: process.env.REDIS_HOST,
+//       port: process.env.REDIS_PORT 
+//     }
+//   });
+//   redisClient.connect()
+//   redisClient.ping()
 module.exports = redisClient
