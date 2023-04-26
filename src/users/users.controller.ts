3b9d21452be2ae7458f89f7e02fdbf9a7c680a35
rodinkim <xday6666@gmail.com>
2023-04-26 21:23:54 +0900

    [feat] signup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Post, Controller, UsePipes, ValidationPipe, HttpCode } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignupReqeustDto } from './dtos/signup.dto';
 
-@Controller('users')
-export class UsersController {}
+
+@Controller('user')
+export class UsersController {
+    constructor(private usersService: UsersService){
+
+    }
+    @Post('signup')
+    async signup(@Body() data: SignupReqeustDto){
+      return this.usersService.signup(data);
+    }
+
+
+
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users.entity';
+import mysqlDataSource from 'src/config/typeorm-cli.config';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users], mysqlDataSource)],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
