4f401bab33150b40a7a48efdb21e713f9bec81f7
rodinkim <xday6666@gmail.com>
2023-05-01 15:17:05 +0900

    [feat] cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors:true});
   // const configService = app.get(ConfigService);
   // const DBhost = configService.get('DATABASE_URL');
   // const DBicon = configService.get('DATABASE_ICON');
