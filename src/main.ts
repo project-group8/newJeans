93714ec9caa85885d9881c667ebe92565ec1e112
rodinkim <xday6666@gmail.com>
2023-05-04 15:46:33 +0900

    [feat] cors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
-    credentials: true,
-    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+    // origin: true,
+    // credentials: true,
+    // exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    exposedHeaders: ['Authorization'],
   });
 
   app.setGlobalPrefix('api');
